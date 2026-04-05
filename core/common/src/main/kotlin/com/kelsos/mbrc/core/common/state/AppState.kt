9966af560e4dc5524a62c645e2aa030f2484591c
Omkar Chandorkar <gotenksIN@aospa.co>
2026-04-05 19:43:00 +0530

    fix: clear stale lyrics when the playing track changes

--- core/common/src/main/kotlin/com/kelsos/mbrc/core/common/state/AppState.kt
+++ core/common/src/main/kotlin/com/kelsos/mbrc/core/common/state/AppState.kt
@@ -47,6 +47,18 @@ class AppState : AppStatePublisher {
   }
 
   override fun updatePlayingTrack(track: TrackInfo) {
+    val previousTrack = _playingTrack.value
+    val trackChanged =
+      previousTrack.path != track.path ||
+        previousTrack.title != track.title ||
+        previousTrack.artist != track.artist ||
+        previousTrack.album != track.album ||
+        previousTrack.year != track.year
+
+    if (trackChanged) {
+      _lyrics.value = emptyList()
+    }
+
     _playingTrack.value = track
   }
 
