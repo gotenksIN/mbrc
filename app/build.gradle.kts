af52603a9b5a73589157efc4339ecb85aadbac50
Omkar Chandorkar <gotenksIN@aospa.co>
2026-04-05 19:24:53 +0530

    chore: remove LeakCanary from CI builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -390,9 +390,6 @@ dependencies {
   androidTestImplementation(libs.kotlin.coroutines.test)
   androidTestImplementation(libs.mockk.android)
 
-  if (!isCiBuild) {
-    debugImplementation(libs.squareup.leakcanary)
-  }
   debugImplementation(libs.androidx.fragment.testing)
   debugImplementation(libs.androidx.compose.ui.tooling)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
